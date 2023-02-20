c18c3d19cae9769b221b60c62de1a51fa34d8556
Windows 11 <leonardo_pischetz@hotmail.com>
2023-02-20 01:43:14 -0300

    Sumando la ultima Funcionalidad faltante

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,12 @@
 import SearchBar from './SearchBar';
 import { Link } from 'react-router-dom';
 
+const randomChar = (event) =>{
+    const random = Math.floor(Math.random()*826);
+    console.log(random);
+    return random;
+}
+
 const Nav = ({ onSearch, logout }) => {
 
   return(
@@ -9,6 +15,7 @@ const Nav = ({ onSearch, logout }) => {
             <Link to = "/home">Home</Link>
             <Link to = "/favorites">My Favorites</Link>
             <button onClick={logout}>Logout</button>
+            <button onClick={() => onSearch(randomChar())}>Random</button>
             <SearchBar onSearch ={onSearch} />    
         </nav>
     )
