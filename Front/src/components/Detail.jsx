3528d4234c122b269556504e2684a91812e0841c
Windows 11 <leonardo_pischetz@hotmail.com>
2023-03-03 22:38:29 -0300

    Un poco de estilos

--- Front/src/components/Detail.jsx
+++ Front/src/components/Detail.jsx
@@ -1,6 +1,30 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
+const Divgral = styled.div`
+font-family: "Comic Neue";
+background-color: rgba(255, 255, 255, 0.7);
+font-size: 3vw;
+border-radius: 1%;
+margin-top: 2%;
+color: black;
+`
+const Boton = styled.button`
+width: 25%;
+color: rgb(0, 200, 80);
+font-family: "Comic Neue";
+font-size: 2vw;
+margin: auto;
+border-radius: 5%;
+`
+const Divdatos = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+align-items: center;
+padding: 1%;
+`
 
 const Detail = () =>{
 
@@ -29,15 +53,21 @@ const Detail = () =>{
       }, [detailId]);
     
     return(
-        <div>
-            <button onClick={handleClick}>Back to Home</button>
-            <h1>{character?.name}</h1>
+        <Divgral>
+          <Divdatos>
+            <div>
+            <h2>{character?.name}</h2>
             <p>{character?.status}</p>
             <p>{character?.specie}</p>
             <p>{character?.gender}</p>
             <p>{character?.origin?.name}</p>
+            </div>
+            <div>
             <img src={character?.image} alt={character.name} />
-        </div>
+            </div>
+            </Divdatos>
+            <Boton onClick={handleClick}>Back to Home</Boton>
+        </Divgral>
     )
 }
 
